c6ee81f0eea55433f0cd2e1c73a8a449c06ee7cc
RivaRux217 <58639413+RivaRux217@users.noreply.github.com>
2026-02-28 20:10:33 +0100

    Add files via upload

--- compilar.sh
+++ compilar.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Solicitamos un nodo con 64 cores y 256 GB de memoria durante 2 horas
 # Cambiar no sbatch para menos tempo e memoria para que nos den acceso máis fácilmente. Facer unha prueba con pouco
-#SBATCH -n 1 -c 64 -t 02:00:00 --mem=256G
+#SBATCH -n 1 -c 64 -t 00:15:00 --mem=16G
 # Ponemos nombre a nuestro trabajo para poder identificarlo.
 # ATENCIÓN - Debes sustituir el NN por el número de equipo.
-#SBATCH --job-name p1acgNN
+#SBATCH --job-name p1acg1013
 
 # Sustituir los valores de Di y Li por los calculados para la realización de la práctica.
 
